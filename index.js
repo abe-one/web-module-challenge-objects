60b2b8a3320941c9a092c6b2cac74394443f6d4d
abe-one <77995299+abe-one@users.noreply.github.com>
2021-02-18 15:21:43 -0600

    Completed Task4, the '(no function needed)' notes are very helpful

--- index.js
+++ index.js
@@ -86,6 +86,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
+console.log(reviews[5].feedback);
 
 
 
@@ -95,7 +96,13 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
+reviews.push({
+  name: `Ficty`,
+  rating: 3.14,
+  feedback: `I'm Ficty and I approve this review`
+})
 
+console.log(reviews);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -103,6 +110,7 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
+
 
 
 
